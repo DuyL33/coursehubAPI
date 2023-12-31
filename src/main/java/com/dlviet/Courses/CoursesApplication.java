6c1adf572534278bd10804f29c2c8ce9d51972b8
Dle33 <lduyviet999@gmail.com>
2023-12-31 17:29:02 -0500

    exclude mongo auto config

--- src/main/java/com/dlviet/Courses/CoursesApplication.java
+++ src/main/java/com/dlviet/Courses/CoursesApplication.java
@@ -6,13 +6,15 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 
+import org.springframework.boot.autoconfigure.data.mongo.MongoDataAutoConfiguration;
+import org.springframework.boot.autoconfigure.mongo.MongoAutoConfiguration;
 import org.springframework.context.annotation.Bean;
 import org.springframework.core.env.Environment;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
-@SpringBootApplication
+@SpringBootApplication(exclude = {MongoAutoConfiguration.class, MongoDataAutoConfiguration.class})
 @RestController
 public class CoursesApplication {
 
